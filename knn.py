d4cb05e7d25bfae94c47ec88895de87ad19ad492
Mlee3333 <166416296+Mlee3333@users.noreply.github.com>
2024-06-09 10:24:47 +0100

    Update knn.py

--- knn.py
+++ knn.py
@@ -11,13 +11,25 @@ X = Data.drop("species",axis=1)
 le = LabelEncoder()
 y = le.fit_transform(Data["species"])
 
-Xtrain,Xtest,ytrain,ytest = train_test_split(X,y)
+Xtrain,Xtest,ytrain,ytest = train_test_split(X,y,test_size=0.2,random_state=4)
 scaler = StandardScaler()
 scaler.fit_transform(Xtrain)
 
 from sklearn.neighbors import KNeighborsClassifier
+#finding the best k value
+from sklearn.metrics import f1_score
+f1 = []
+for i in range (2,10):
+    KNN = KNeighborsClassifier(n_neighbors=i)
+    KNN.fit(Xtrain,ytrain)
+    pred = KNN.predict(Xtrain)
+    f1.append(f1_score(ytrain,pred,average="macro"))
 
-KNN = KNeighborsClassifier(n_neighbors=5)
+plt.plot(range(2,10),f1)
+plt.plot(range(2,10),[max(f1)for i in range(2,10)],"r")
+plt.show()
+     
+KNN = KNeighborsClassifier(n_neighbors=6)
 KNN.fit(Xtrain,ytrain)
 trainpred = KNN.predict(Xtrain)
 
